0906498b25a1a8e4a77e8d8768b3e5c0b725de9c
jaydatt <jaydat@logisticinfotech.co.in>
2024-10-11 16:03:00 +0530

    log in axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL , // Fallback URL
 });
-
+console.log( process.env.NEXT_PUBLIC_BASE_URL);
 export default axiosInstance;
