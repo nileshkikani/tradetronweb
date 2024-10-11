0a828d2f562088641b1f6ad993f0b169ce3e8046
jaydatt <jaydat@logisticinfotech.co.in>
2024-10-11 13:51:54 +0530

    fix: axiosInstance base url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.BASE_URL || 'http://192.168.0.248:8000/api/',
+    baseURL: process.env.BASE_URL,
     // headers: {
     //     'Content-Type': 'application/json'
     //   },
