296767a7dbbce48c171cf02ee9261ebd96e6d401
jaydatt <jaydat@logisticinfotech.co.in>
2024-10-22 20:20:08 +0530

    UPDATE: reusable fields in broker page

--- src/content/DashboardPages/brokers/index.js
+++ src/content/DashboardPages/brokers/index.js
@@ -31,107 +31,38 @@ const DashboardBrokersContent = () => {
         });
     };
 
+    const renderTextField = (name, label, type = 'text') => (
+        <TextField
+            name={name}
+            label={label}
+            type={type}
+            fullWidth
+            margin="normal"
+            onChange={handleChange}
+        />
+    );
+
     const renderFields = () => {
-        if (selectedBroker === 'angel') {
-            return (
-                <>
-                    <TextField
-                        name="CLIENT_CODE"
-                        label="CLIENT CODE"
-                        fullWidth
-                        margin="normal"
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        name="PASSWORD"
-                        label="PASSWORD"
-                        type="password"
-                        fullWidth
-                        margin="normal"
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        name="TOTP"
-                        label="TOTP"
-                        fullWidth
-                        margin="normal"
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        name="API_KEY"
-                        label="API KEY"
-                        fullWidth
-                        margin="normal"
-                        onChange={handleChange}
-                    />
-                </>
-            );
-        } else if (selectedBroker === 'kotak') {
-            return (
-                <>
-                    <TextField
-                        name="consumer_key"
-                        label="Consumer Key"
-                        fullWidth
-                        margin="normal"
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        name="consumer_secret"
-                        label="Consumer Secret"
-                        fullWidth
-                        margin="normal"
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        name="username"
-                        label="Username"
-                        fullWidth
-                        margin="normal"
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        name="password"
-                        label="Password"
-                        type="password"
-                        fullWidth
-                        margin="normal"
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        name="neo_fin_key"
-                        label="Neo Fin Key"
-                        fullWidth
-                        margin="normal"
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        name="mobile_no"
-                        label="Mobile No"
-                        fullWidth
-                        margin="normal"
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        name="login_password"
-                        label="Login Password"
-                        type="password"
-                        fullWidth
-                        margin="normal"
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        name="mpin"
-                        label="MPIN"
-                        type="password"
-                        fullWidth
-                        margin="normal"
-                        onChange={handleChange}
-                    />
-                </>
-            );
-        }
-        return null;
+        const fields = {
+            angel: [
+                { name: 'CLIENT_CODE', label: 'CLIENT CODE' },
+                { name: 'PASSWORD', label: 'PASSWORD', type: 'password' },
+                { name: 'TOTP', label: 'TOTP' },
+                { name: 'API_KEY', label: 'API KEY' },
+            ],
+            kotak: [
+                { name: 'consumer_key', label: 'Consumer Key' },
+                { name: 'consumer_secret', label: 'Consumer Secret' },
+                { name: 'username', label: 'Username' },
+                { name: 'password', label: 'Password', type: 'password' },
+                { name: 'neo_fin_key', label: 'Neo Fin Key' },
+                { name: 'mobile_no', label: 'Mobile No' },
+                { name: 'login_password', label: 'Login Password', type: 'password' },
+                { name: 'mpin', label: 'MPIN', type: 'password' },
+            ],
+        };
+
+        return fields[selectedBroker]?.map(field => renderTextField(field.name, field.label, field.type));
     };
 
     return (
@@ -143,30 +74,16 @@ const DashboardBrokersContent = () => {
                 <Button
                     variant="outlined"
                     onClick={() => handleOpen('angel')}
-                    startIcon={
-                        <Image
-                            src="/angel.png"
-                            alt="Angel"
-                            width={60}
-                            height={60}
-                        />
-                    }
-                    endIcon={selectedBroker === 'angel' ? <CheckCircleIcon color="success" /> : null} // Show checkmark if selected
+                    startIcon={<Image src="/angel.png" alt="Angel" width={60} height={60} />}
+                    endIcon={selectedBroker === 'angel' ? <CheckCircleIcon color="success" /> : null}
                 >
                     Angel One
                 </Button>
                 <Button
                     variant="outlined"
                     onClick={() => handleOpen('kotak')}
-                    startIcon={
-                        <Image
-                            src="/kotak_neo.png"
-                            alt="Kotak"
-                            width={60}
-                            height={60}
-                        />
-                    }
-                    endIcon={selectedBroker === 'kotak' ? <CheckCircleIcon color="success" /> : null} // Show checkmark if selected
+                    startIcon={<Image src="/kotak_neo.png" alt="Kotak" width={60} height={60} />}
+                    endIcon={selectedBroker === 'kotak' ? <CheckCircleIcon color="success" /> : null}
                 >
                     Kotak Neo
                 </Button>
